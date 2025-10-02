6b5c3c8e1623ea98a89abd7bb291012c7a1ce067
macadura <macadura@hotmail.com>
2025-10-01 23:48:21 -0300

    ...

---
+++ src/data/topicos-ajuda.js
@@ -0,0 +1,24 @@
+const JSON = [
+  {
+    titulo: "Problemas para encontrar o produto desejado",
+    descricao: "Use a barra de busca para procurar pelo nome ou código. Você também pode navegar pelas categorias.",
+  },
+  {
+    titulo: "A barra de comentário não está aparecendo",
+    descricao: "...",
+  },
+  {
+    titulo: "Não estou conseguindo fazer login",
+    descricao: "...",
+  },
+  {
+    titulo: "Como redefinir a senha",
+    descricao: "...",
+  },
+  {
+    titulo: "Os comentários não aparecem da tela de favoritos",
+    descricao: "...",
+  },
+];
+
+export default JSON;
---
+++ src/services/ajuda-service.js
@@ -0,0 +1,9 @@
+import TopicosAjuda from "../data/topicos-ajuda";
+
+const AjudaService = {
+  obterTopicosAjuda() {
+    return TopicosAjuda;
+  },
+};
+
+export default AjudaService;
